bf3d3dea1befad1b5980524e2ac8baf4d63cc5e7
Snehal Shelar <76094243+snehal2196@users.noreply.github.com>
2021-11-26 20:18:41 +0530

    latest changes

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import React, { useState } from 'react'
 import Alert from './components/Alert';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route,
+//   Link
+// } from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState('light')
@@ -39,21 +39,21 @@ function App() {
   }
   return (
     <>
-      <Router>
+      {/* <Router> */}
         <Navbar title='TextUtils' mode={mode} toggleMode={toggleMode} />
         <Alert alert={alert} />
         <div className="container my-4">
 
-          <Routes>
-            <Route exact path='/about' element={<About mode={mode}/>} />
+          {/* <Routes> */}
+            {/* <Route exact path='/about' element={<About mode={mode}/>} /> */}
 
-            <Route exact path='/' element={<TextForm heading="Try TextUtils - Word Counter, Character Counter, Remove Space" mode={mode} showAlert={showAlert} />} />
-            {/* <TextForm heading="Enter the text below to analyze" mode={mode} showAlert={showAlert} /> */}
+            {/* <Route exact path='/' element={<TextForm heading="Try TextUtils - Word Counter, Character Counter, Remove Space" mode={mode} showAlert={showAlert} />} /> */}
+            <TextForm heading="Try TextUtils - Word Counter, Character Counter, Remove Space" mode={mode} showAlert={showAlert} />
 
-          </Routes>
+          {/* </Routes> */}
 
         </div>
-      </Router>
+      {/* </Router> */}
     </>
   );
 }
